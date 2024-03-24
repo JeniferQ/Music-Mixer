f29afcecb5cd84379fc3a8d917b860dc8463a00b
JeniferQ <144718154+JeniferQ@users.noreply.github.com>
2024-03-24 15:57:01 -0400

    Change functions and CD
    In this commit, a few functions were renamed to make their names shorter. The CD drop zone was removed.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ const musicTracks = document.querySelectorAll(".track-selector img"),
     pauseButton = document.querySelectorAll('.pause'),
     restartButton = document.querySelectorAll('.restart'),
     audioElement = document.querySelector('#audio'),
-    cdDropZone = document.querySelector('.cd-zone'),
 	playerDropZone = document.querySelector('.player-zone');
 
-let draggedTrack, draggedCD;
+let draggedPiece;
 
 function startThisAudio() {
     let currentSrc = `audio/${this.dataset.trackref}.mp3`;
@@ -19,49 +18,42 @@ function startThisAudio() {
     console.log('user is playing the', this.dataset.trackref);
 }
 
-function playThisAudio() { 
+function play() { 
     audioElement.play(); 
 }
 
-function pauseThisAudio() { 
+function pause() { 
     audioElement.pause(); 
 }
 
-function restartThisAudio() { 
+function restart() { 
     audioElement.currentTime = 0; 
     playThisAudio(); 
 }
 
-function startDrag() { 
-	console.log('user is dragging this audio');
-	draggedTrack = this;
+function startDrag() {
+    console.log('user started dragging an audio');
+    draggedPiece = this;
+}
+    
+function dragOver(e) {
+    e.preventDefault();
+    console.log('user is dragging an audio over the zone');
+}
+    
+function endDrag(e) {
+    e.preventDefault();
+    console.log('user has dropped the audio');
 }
 
-function dragOver(e) { 
-	e.preventDefault(); 
-	console.log('this music is being dragged over the player'); 
-}
-
-function endDrag(e) { 
-	e.preventDefault();
-	console.log('the music was dropped on the player');
-
-    var audio = document.createElement("audio");
-    audio.src = `audio/${this.dataset.trackref}.mp3`;
-
-	this.appendChild(audio);
-}
 
 musicTracks.forEach(track => track.addEventListener('click', startThisAudio));
 musicTracks.forEach(track => track.addEventListener("dragstart", startDrag));
 document.getElementById("audio").loop = true;
 
-playButton.forEach(button => button.addEventListener('click', playThisAudio));
-pauseButton.forEach(button => button.addEventListener('click', pauseThisAudio));
-restartButton.forEach(button => button.addEventListener('click', restartThisAudio));
-
-cdDropZone.addEventListener("dragover", dragOver);
-cdDropZone.addEventListener("drop", endDrag);
+playButton.forEach(button => button.addEventListener('click', play));
+pauseButton.forEach(button => button.addEventListener('click', pause));
+restartButton.forEach(button => button.addEventListener('click', restart));
 
 playerDropZone.addEventListener("dragover", dragOver);
 playerDropZone.addEventListener("drop", endDrag);
